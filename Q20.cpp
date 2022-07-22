e6145a1bedc4ed9f2e2470607360eb0688cf5327
DISCRETE STRUCTURE..18102 <108319130+Suraj761985@users.noreply.github.com>
2022-07-22 18:15:04 +0530

    Add files via upload

--- Q20.cpp
+++ Q20.cpp
@@ -1,34 +1,28 @@
-#include <bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
-void DFS(list<int> t[], int node, int parent) {
-   int flag = 0;
-   for (auto ir : t[node]) {
-      if (ir != parent) {
-         flag = 1;
-         DFS(t, ir, node);
-      }
-   }
-   if (flag == 0)
-      cout<<node<<"\t";
+
+int calcNodes(int m, int I)
+{
+	int result = 0;
+
+	result = I * (m - 1) + 1;
+
+	return result;
 }
-int main() {
-   list<int> t[1005];
-   pair<int, int> edges[] = {
-      { 1, 2 },
-      { 1, 3 },
-      { 2, 4 },
-      { 3, 5 },
-      { 3, 6 },
-      { 3, 7 },
-      { 6, 8 }
-   };
-   int cnt = sizeof(edges) / sizeof(edges[0]);
-   int node = cnt + 1;
-   for (int i = 0; i < cnt; i++) {
-      t[edges[i].first].push_back(edges[i].second);
-      t[edges[i].second].push_back(edges[i].first);
-   }
-   cout<<"Leaf nodes of the tree are:\n";
-   DFS(t, 1, 0);
-   return 0;
+
+int main()
+{
+	int m,I,N;
+
+	cout << "\nEnter the maximum no. of children in full m-ary tree: ";
+	cin >> m;
+
+	cout << "Enter the number of internal vertices: ";
+	cin >> I;
+
+	N = calcNodes(m, I);
+	cout << "Number of Leaf Nodes in the full m-ary tree: " << N;
+
+	return 0;
 }
